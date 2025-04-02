85fe9602ee26dbecc42e191a1f21c3deda865381
Alejandro_Sevilla <alejandro.murillo.sevilla@gmail.com>
2025-04-02 18:03:56 +0200

    nanvar-cambios

--- components/lista-gargolas/link-gargolas.jsx
+++ components/lista-gargolas/link-gargolas.jsx
@@ -4,15 +4,10 @@ function LinkGargola({nombre,url}) {
 
     
     return (
-<<<<<<< HEAD
         <li className='bg-blue-500 px-4 py-2'>
-=======
-        <li className='bg-red-500 px-4 py-2'>
->>>>>>> 36b902dab1df49b4cd0b9328bfc01b0621daf557
             <Link href={`/gargolas/${url}`} >{nombre} </Link> 
         </li>
     ); 
 }
 
 export default LinkGargola;
-
