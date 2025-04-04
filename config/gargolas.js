f240638a3b23544fd85aecc4a05f8f99070e04e1
Alejandro_Sevilla <44191399+AleSevilla@users.noreply.github.com>
2025-04-04 13:22:12 +0200

    añadidas localizacionees

--- config/gargolas.js
+++ config/gargolas.js
@@ -3,6 +3,7 @@ export const gargolaslist = [
   {
     id: 1,
     nombre: 'Gárgolas de la Catedral',
+    zona: "Catedral",
     url: "sombrio-centinela",
     morfologia: "Figura demoníaca alada",
     tipologia: "Decorativa",
@@ -255,175 +256,50 @@ export const gargolaslist = [
     ],
   },
 
-  {
-    id: 17,
-    nombre: '',
-    url: "",
-    morfologia: "",
-    tipologia: "",
-    localizacion: "3 ",
-    descripcion: "",
-    imagePath: "",
-    galeria: [
-      //  { src: '', alt: 'imagen 1', },
-    ],
-  },
-
-  {
-    id: 18,
-    nombre: '',
-    url: "",
-    morfologia: "",
-    tipologia: "",
-    localizacion: " ",
-    descripcion: "",
-    imagePath: "",
-    galeria: [
-      //  { src: '', alt: 'imagen 1', },
-    ],
-  },
-
-  {
-    id: 19,
-    nombre: '',
-    url: "",
-    morfologia: "",
-    tipologia: "",
-    localizacion: " ",
-    descripcion: "",
-    imagePath: "",
-    galeria: [
-      //  { src: '', alt: 'imagen 1', },
-    ],
-  },
-
-
-
-  /*
-,
-  {
-    id: "2",
-    nombre: 'Gargolas del Ayuntamiento',
-    url: "Gargola-simpatica-del-ayuntamiento",
-    morfologia: "Quimera con pico de ave",
-    tipologia: "Drenaje",
-    localizacion: "Archivo de Indias",
-    descripcion: "El Heraldo del Viento se alza sobre el Archivo de Indias, con su pico abierto como si estuviera a punto de lanzar un grito. Sus alas pétreas muestran erosión por siglos de lluvia. Se dice que en noches de viento, su silueta parece cobrar vida entre las sombras de la luna.",
-    imagePath:"",
-    galeria: [
-      { src: '/imagen1/1270/720', alt: 'imagen 1' },
-    ]
-  },
-  {
-    id: "3",
-    nombre: 'Gargolas del Convento Santa Paula',
-    url: "bestia-del-giraldillo",
-    morfologia: "Híbrido de reptil y felino",
-    tipologia: "Drenaje",
-    localizacion: "Catedral de Sevilla",
-    descripcion: "La Bestia del Giraldillo descansa cerca de la torre, con su cuerpo alargado y musculoso. Su boca, que vierte agua en días de lluvia, parece congelada en un rugido eterno. Según los mitos, por las noches desciende de su pedestal para recorrer los tejados de la ciudad.",
-    imagePath:"",
-    galeria: [
-      { src: '/imagen1/1270/720', alt: 'imagen 1' },
-    ]
-  },
-
-  {
-    id: "4",
-    nombre: 'Gargolas de la antigua capilla hospital San lazaro',
-    url: "catedral",
-    morfologia: "Figura humana alada",
-    imagePath: '/catedral',
-    tipologia: "Decorativa",
-    localizacion: "Palacio Arzobispal",
-    descripcion: "Con una postura vigilante, el Espíritu del Arroyo adorna la fachada del Palacio Arzobispal. Su rostro es sereno pero enigmático, como si escondiera secretos del pasado. Los ancianos cuentan que su estatua llora gotas de agua cuando la ciudad sufre grandes desgracias.",
-
-    imagePath:"",
-    galeria: [{ src: "P1160226.jpg", alt: "catedral 1" },
-    { src: "P1160230.jpg", alt: "catedral 1" },
-    { src: "P1160236.jpg", alt: "catedral 1" },
-    { src: "P1160237.jpg", alt: "catedral 1" },
-    { src: "_1130645.jpg", alt: "catedral 1" },
-    { src: "_1130652.jpg", alt: "catedral 1" },
-    { src: "_1130657.jpg", alt: "catedral 1" },
-    { src: "_1130687.jpg", alt: "catedral 1" },
-    { src: "_1130690.jpg", alt: "catedral 1" },
-    { src: "_1130691.jpg", alt: "catedral 1" },
-    { src: "_1130696.jpg", alt: "catedral 1" },
-    { src: "_1130707.jpg", alt: "catedral 1" },
-    { src: "_1130709.jpg", alt: "catedral 1" },
-    { src: "_1130710.jpg", alt: "catedral 1" },
-    { src: "_1130713.jpg", alt: "catedral 1" },
-    { src: "_1130715.jpg", alt: "catedral 1" },
-    { src: "_1130724.jpg", alt: "catedral 1" },
-    { src: "_1130725.jpg", alt: "catedral 1" },
-    { src: "_1130726.jpg", alt: "catedral 1" },
-    { src: "_DSF3313.jpg", alt: "catedral 1" },
-    { src: "_DSF3314.jpg", alt: "catedral 1" },
-    { src: "_DSF3315.jpg", alt: "catedral 1" },
-    { src: "_DSF3461.jpg", alt: "catedral 1" },
-    { src: "_DSF3464.jpg", alt: "catedral 1" },
-    { src: "_DSF3472.jpg", alt: "catedral 1" },
-    { src: "_DSF3587.jpg", alt: "catedral 1" },
-    { src: "_DSF3807.jpg", alt: "catedral 1" },
-    { src: "_DSF4118.jpg", alt: "catedral 1" },
-    { src: "_DSF4323.jpg", alt: "catedral 1" },
-    { src: "_DSF4369.jpg", alt: "catedral 1" },
-    { src: "_DSF5911.jpg", alt: "catedral 1" },
-    { src: "_DSF5923.jpg", alt: "catedral 1" },
-    { src: "_DSF5929.JPG", alt: "catedral 1" },
-    { src: "_DSF5971.jpg", alt: "catedral 1" },
-    { src: "_DSF9935.jpg", alt: "catedral 1" },]
-  }
-
-  ,
-  {
-    id: "5",
-    nombre: 'Gargolas del pabellon real',
-    url: "gargola-danzarina",
-    morfologia: "Figura humana alada",
-    tipologia: "Decorativa",
-    localizacion: "Palacio Arzobispal",
-    descripcion: "Con una postura vigilante, el Espíritu del Arroyo adorna la fachada del Palacio Arzobispal. Su rostro es sereno pero enigmático, como si escondiera secretos del pasado. Los ancianos cuentan que su estatua llora gotas de agua cuando la ciudad sufre grandes desgracias.",
-    imagePath:"",
-    galeria: [
-      { src: '/imagen1/1270/720', alt: 'imagen 1' },
-    ]
-  }
-*/
-
 ]
+
 
 
 export const localizaciones = [
   {
     lugar: 'Catedral',
+    imagen: 'catedral.webp',
     nombre: "Catedral de Sevilla",
-    url: 'catedral'
+    url: 'catedral',
+    gargolas: 5,
   },
   {
     lugar: 'Ayuntamiento',
+    imagen: 'ayuntamiento.webp',
     nombre: "Ayuntamiento de Sevilla",
-    url: 'ayuntamiento'
+    url: 'ayuntamiento',
+    gargolas: 2,
   },
   {
-    lugar: 'frabrica',
+    lugar: 'fabrica',
+    imagen: 'fabrica.webp',
     nombre: "Antigua Fábrica de Tabacos",
-    url: 'fabrica-de-tabacos'
+    url: 'fabrica-de-tabacos',
+    gargolas: 7,
   },
   {
     lugar: 'parlamento',
-    nombre: "Antigua Hospital de las Cinco Llagas",
-    url: 'parlamento'
+    imagen: 'parlamento.webp',
+    nombre: "Antiguo Hospital de las Cinco Llagas",
+    url: 'parlamento',
+    gargolas: 3,
   }, {
     lugar: 'santelmo',
+    imagen: 'santelmo.webp',
     nombre: "Palacio de San Telmo",
-    url: 'san-telmo'
+    url: 'san-telmo',
+    gargolas: 1,
   }, {
-    lugar: 'isidoro',
-    nombre: "Antigua Hospital de las Cinco Llagas",
-    url: 'parlamento'
+    lugar: 'sanisidoro',
+    imagen: 'sanisidoro.webp',
+    nombre: "Parroquia deSan Isidoro",
+    url: 'parlamento',
+    gargolas: 4,
   }
-
 
 ]
--- config/menus.js
+++ config/menus.js
@@ -1,6 +1,7 @@
 export const menuslist = [
     { href: "/", title: "Inicio" },
     { href: '/historia', title: 'Historia' },
+    { href: '/localizaciones', title: 'Edificios' },
     { href: '/gargolas', title: 'Listado de gárgolas' },
     { href: '/contacto', title: 'Contacto' },
 
